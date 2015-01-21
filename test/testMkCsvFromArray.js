1769eff44515e073912dac28ea95cdf194467327
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-01-21 09:36:48 +0100

    Adds test for output

--- test/testMkCsvFromArray.js
+++ test/testMkCsvFromArray.js
@@ -3,11 +3,20 @@
 var mkCsvFromArray = require('../lib/mkcsvfromarray');
 var assert = require('assert');
 
-describe('mkCsvFromArray - inputs', function() {
+describe('mkCsvFromArray - input', function() {
   it('it requires an array', function() {
     var arr = false;
     var csv = mkCsvFromArray(arr);
-    var errMsg = new Error('Invalid input. Expected an array.');
-    assert.equal(csv, errMsg);
+    var errMsg = 'Invalid input. Expected an array.';
+    assert.equal(csv.message, errMsg);
+  });
+});
+
+describe('mkCsvFromArray - output', function() {
+  it('it returns as expected', function() {
+    var arr = ['Ole', 'Dole', 'Doffen'];
+    var csv = mkCsvFromArray(arr);
+    var output = '"Ole","Dole","Doffen"\r\n';
+    assert.equal(csv, output);
   });
 });
